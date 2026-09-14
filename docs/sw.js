bf9a9eaee7f3453d423a3de5c0b8d07fb383aab5
gekoncaros <gekoncaros@gmail.com>
2026-09-14 05:47:54 -0700

    Refresh Pages PWA cache to v6

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE='delegacje-flota-demo-v5';
-const ASSETS=['./','./index.html','./style.css','./accounting.css','./app.js','./accounting.js','./manifest.webmanifest'];
+const CACHE='delegacje-flota-demo-v6';
+const ASSETS=['./','./index.html','./style.css','./accounting.css','./superadmin.css','./app.js','./accounting.js','./superadmin.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(resp=>{if(resp.ok){const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
