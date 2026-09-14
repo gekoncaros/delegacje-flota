133b9866c8308d1e55c07fcb702956e3fac64b45
gekoncaros <gekoncaros@gmail.com>
2026-09-14 06:17:07 -0700

    Add logout API endpoint

---
+++ public/api/auth/logout.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+require dirname(__DIR__) . '/_api.php';
+
+require_method('POST');
+$_SESSION = [];
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'] ?? '', (bool)$params['secure'], (bool)$params['httponly']);
+}
+session_destroy();
+api_response(['ok' => true]);
