c1247f72529d84ef1fd48e0b0818ad255e040fda
gekoncaros <gekoncaros@gmail.com>
2026-09-18 00:23:37 +0200

    Add production foundation: server/config.example.php

---
+++ server/config.example.php
@@ -0,0 +1,5 @@
+<?php
+return [
+ 'db'=>['dsn'=>'mysql:host=localhost;dbname=delegacje;charset=utf8mb4','user'=>'delegacje','pass'=>'CHANGE_ME'],
+ 'app'=>['base_url'=>'https://delegacje.example.pl','session_name'=>'delegacje_session']
+];
